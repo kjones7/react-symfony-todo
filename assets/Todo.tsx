5b76986ce669297642d000b27879e5aaa646f2a9
Kyle Jones <mynameiskyle242@gmail.com>
2023-09-23 17:08:36 -0400

    Move `NoteCard` component to its own file

--- assets/Todo.tsx
+++ assets/Todo.tsx
@@ -1,31 +1,7 @@
 import React, {ChangeEvent, useEffect, useState} from "react";
-import {Col, Container, Row, Form, Button, Card, InputGroup, Dropdown} from "react-bootstrap";
+import {Button, Col, Container, Form, InputGroup, Row} from "react-bootstrap";
 import 'bootstrap-icons/font/bootstrap-icons.css';
-
-function NoteCard({ id, note, deleteNoteHandler }: { id: number, note: Note, deleteNoteHandler: (key: number) => void }) {
-  return (
-    <Card style={{marginBottom: '1rem'}} className="shadow border-light-subtle">
-      <Card.Body>
-        <Card.Text>{note.content}</Card.Text>
-      </Card.Body>
-      <Card.Footer>
-          <Dropdown>
-            <Dropdown.Toggle variant="link" className="float-end caret-off text-decoration-none text-body">
-              <i className="bi-three-dots-vertical card-footer-btn"></i>
-            </Dropdown.Toggle>
-            <Dropdown.Menu>
-              <Dropdown.Item
-                  href="#"
-                  onClick={() => deleteNoteHandler(id)}
-              >
-                Delete
-              </Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-      </Card.Footer>
-    </Card>
-  );
-}
+import {NoteCard} from "./NoteCard";
 
 function Todo() {
   const [noteToAdd, setNoteToAdd] = useState('');
